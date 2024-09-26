cd061044ee66fb75f4684a54d2088d808f4b1119
DESKTOP-CMUM67H\Hintzke Wiktor4TP <wiktorhintzke@gmail.com>
2024-09-26 11:20:42 +0200

    pola edycyjnego i jego etykiety o treści „Tytuł filmu”

--- filmy/src/App.js
+++ filmy/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="mb-3">
+        <label for="titleFormControlInput1" class="form-label">tytul filmu</label>
+        <input type="text" class="form-control" id="titleFormControlInput1"/>
+      </div>
     </div>
   );
 }
