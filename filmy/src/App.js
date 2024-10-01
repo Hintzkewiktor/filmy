9908b4d5df8990be1fb652e342b463164a57a303
DESKTOP-LCR1M41\Egzamin <wiktorhintzke@gmail.com>
2024-10-01 11:04:09 +0200

    dodanie bootstrapa

--- filmy/src/App.js
+++ filmy/src/App.js
@@ -1,14 +1,36 @@
+import React,{useStatte} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
 function App() {
+  //Stany przechoujace dane formularza
+  const [tytul, setTytyl] = useState("");
+  const [rodzaj, setRodzaj] = useState("");
+
+  //Funkcja obslugujaca przycisk"Dodaj"
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    //wyswietlanie danych formularza konsoli
+    console.log(`Tytuł:${tytul}, Rodzaj: ${rodzaj}`);
+  };
+
   return (
-    <div className="App">
-      <div class="mb-3">
-        <label for="titleFormControlInput1" class="form-label">tytul filmu</label>
-        <input type="text" class="form-control" id="titleFormControlInput1"/>
-      </div>
-    </div>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   );
 }
-
+ 
 export default App;
